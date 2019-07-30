0df45f9d91723744e9b6f3712420a9b11301b5c0
Vicky <1079893731@qq.com>
2019-07-30 17:12:24 +0800

    完成首页导航部分

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,24 +9,37 @@ Page({
   data: {
     // 轮播图数组
     swiperList: [],
+    // 分类导航
+    navCateList: [],
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getSwiperList();
+    this.getNavCateList();
   },
   // 获取轮播图数据
   getSwiperList() {
     request({url: '/home/swiperdata'})
     .then(result=>{
-        console.log(result)
+        // console.log(result)
         this.setData({
           swiperList: result
         })
     })
 
 
+  },
+  // 获取导航数据
+  getNavCateList(){
+    request({url:'/home/catitems'})
+    .then(result=>{
+      console.log(result)
+      this.setData({
+        navCateList:result
+      })
+    })
   }
 
 })
