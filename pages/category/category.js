60471d19aac7ff83d60cbc638bc1099770c8f9bb
Vicky <1079893731@qq.com>
2019-07-31 10:29:18 +0800

    完成分类部分

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,50 @@
 // pages/category/category.js
+import {request} from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList:[],
+    // 右侧产品列表
+    rightGoodsList:[],
+    // 选中的当前索引
+    currentIndex:0,
+    // 设置竖向滚动条位置	
+    scrollTop:0
   },
-
+  // 分类总数据
+  cates:[],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCategories()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取分类数据
+  getCategories(){
+    request({url:"/categories"})
+    .then(result=>{
+      console.log(result)
+      this.cates=result;
+      const leftMenuList=this.cates.map(v=>({cat_id:v.cat_id,cat_name:v.cat_name}))
+      const rightGoodsList=this.cates[0].children;
+      this.setData({
+        leftMenuList,
+        rightGoodsList
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleMenuChange(e){
+    // console.log(e)
+    const {index} = e.currentTarget.dataset;
+    const rightGoodsList=this.cates[index].children;
+    this.setData({
+      currentIndex:index,
+      rightGoodsList,
+      scrollTop:0
+    })
   }
 })
