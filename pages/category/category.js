1f2308d9643e4c9ba95ea59ec4412e49110350f7
Vicky <1079893731@qq.com>
2019-08-01 11:20:49 +0800

    微信存储wx:setStorageSync 保存分类数据

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,5 +1,5 @@
 // pages/category/category.js
-import {request} from "../../request/index.js"
+import { request } from "../../request/index.js"
 Page({
 
   /**
@@ -7,44 +7,79 @@ Page({
    */
   data: {
     // 左侧菜单数据
-    leftMenuList:[],
+    leftMenuList: [],
     // 右侧产品列表
-    rightGoodsList:[],
+    rightGoodsList: [],
     // 选中的当前索引
-    currentIndex:0,
+    currentIndex: 0,
     // 设置竖向滚动条位置	
-    scrollTop:0
+    scrollTop: 0
   },
-  // 分类总数据
-  cates:[],
+  // 接口的返回值 数组格式
+  // 小程序中不建议把没有必要的数据定义在data中，因为内部会把data中的所有的数据都会传递到
+  // 视图层 wxml，容易导致页面特别卡 
+  cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCategories()
+    // 1 获取缓存的数据 用来判断有没有缓存数据
+    const category = wx.getStorageSync("category");
+    console.log(category,'wx.getStorageSync')
+    // 2 判断有没有缓存数据
+    if (!category) {
+      // 没有数据 1 发送请求 2 存入到缓存 
+      this.getCategories();
+    } else {
+      // 有缓冲数据 
+      // 判断数据是否过期 假设过期时间是10s
+      if (Date.now() - category.time > 1000 * 10) {
+        // 过期了 
+        console.log('过期了')
+        this.getCategories();
+      } else {
+        // 获取缓存的数据
+        const categoryData = category.data;
+        // 给全局数据进行赋值
+        this.cates = categoryData;
+        // 左侧菜单要的数据
+        const leftMenuList = this.cates.map(v => ({ cat_id: v.cat_id, cat_name: v.cat_name }))
+        // 右侧大家店的数据
+        const rightGoodsList = this.cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightGoodsList
+        })
+      }
+    }
   },
   // 获取分类数据
-  getCategories(){
-    request({url:"/categories"})
-    .then(result=>{
-      console.log(result)
-      this.cates=result;
-      const leftMenuList=this.cates.map(v=>({cat_id:v.cat_id,cat_name:v.cat_name}))
-      const rightGoodsList=this.cates[0].children;
-      this.setData({
-        leftMenuList,
-        rightGoodsList
+  getCategories() {
+    request({ url: "/categories" })
+      .then(result => {
+        // console.log(result)
+        // 把接口数据赋值给全局变量
+        this.cates = result;
+        // 把数据存入到缓存中
+        wx.setStorageSync("category", { time: Date.now(), data: this.cates })
+        // 左侧菜单要的数据
+        const leftMenuList = this.cates.map(v => ({ cat_id: v.cat_id, cat_name: v.cat_name }))
+        // 右侧大家店的数据
+        const rightGoodsList = this.cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightGoodsList
+        })
       })
-    })
   },
-  handleMenuChange(e){
+  handleMenuChange(e) {
     // console.log(e)
-    const {index} = e.currentTarget.dataset;
-    const rightGoodsList=this.cates[index].children;
+    const { index } = e.currentTarget.dataset;
+    const rightGoodsList = this.cates[index].children;
     this.setData({
-      currentIndex:index,
+      currentIndex: index,
       rightGoodsList,
-      scrollTop:0
+      scrollTop: 0
     })
   }
 })
