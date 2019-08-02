dcccbfb399a81046e683311de18777382404b7f3
Vicky <1079893731@qq.com>
2019-08-02 15:55:51 +0800

    下拉刷新数据

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -13,7 +13,7 @@ Page({
       { id: 2, title: '价格', isActive: false }
     ],
     // 页面要渲染的商品数组
-    goodsList:[]
+    goodsList: []
   },
   queryParams: {
     query: "",
@@ -29,9 +29,9 @@ Page({
   // 页面开始加载就触发
   // 它的形参可以获取到url上的参数
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
     this.queryParams.cid = options.cid;
-    this.getGoodsList()
+    this.getGoodsList();
   },
   // 子组件触发的事件
   handleItemChange(e) {
@@ -51,8 +51,33 @@ Page({
         console.log(result)
         this.totalPages = Math.ceil(result.total / this.queryParams.pagesize)
         this.setData({
-          goodsList:result.goods
+          // 拼接商品上拉数据
+          goodsList: [...this.data.goodsList, ...result.goods]
         })
+        wx.stopPullDownRefresh();
       })
+  },
+  //监听用户上拉触底事件
+  onReachBottom() {
+    // console.log('监听用户上拉触底事件')
+    if (this.queryParams.pagenum >= this.totalPages) {
+      // console.log('没有下一页数据')
+      wx.showToast({
+        title: '没有更多数据了',
+        icon: 'none',
+      });
+
+    } else {
+      this.queryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+  // 下拉刷新事件
+  onPullDownRefresh() {
+    this.queryParams.pagenum = 1;
+    this.setData({
+      goodsList: []
+    })
+    this.getGoodsList()
   }
 })
