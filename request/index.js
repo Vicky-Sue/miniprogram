ecf38663f4d9ebe478944750977d791e6741bb65
Vicky <1079893731@qq.com>
2019-08-02 18:12:42 +0800

    实现全局的正在加载的图标效果

--- request/index.js
+++ request/index.js
@@ -1,17 +1,29 @@
-export const request = (params)=>{
+let ajaxTimes = 0
+
+export const request = (params) => {
+    ajaxTimes++;
+    // 显示加载
+    wx.showLoading({ title: '加载中', })
     // 公共的接口前缀
-    const baseUrl="https://api.zbztb.cn/api/public/v1";
-    return new Promise((resolve,reject)=>{
+    const baseUrl = "https://api.zbztb.cn/api/public/v1";
+    return new Promise((resolve, reject) => {
         wx.request({
             ...params,
-            url:baseUrl+params.url,
+            url: baseUrl + params.url,
             success: (result) => {
                 resolve(result.data.message)
             },
             fail: (err) => {
                 reject(err)
             },
+            complete: () => {
+                ajaxTimes--;
+                if (ajaxTimes === 0) {
+                    wx.hideLoading()
+                    // console.count('当前执行次数')
+                }
+
+            }
         });
-          
     })
 }
